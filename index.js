736eb9235e1c679b9d4bd20d85306ead491960c6
joecooper1 <joecooper2991@gmail.com>
2020-01-23 18:26:27 +0000

    hexagon fiddling

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const a = document.querySelectorAll(".normal");
 
 //Making squares
 
-const baseNum = Math.random() * 150;
-const baseNum2 = Math.random() * 150;
-const baseNum3 = Math.random() * 150;
+let baseNum = Math.random() * 150;
+let baseNum2 = Math.random() * 150;
+let baseNum3 = Math.random() * 150;
 
-let oppNum1 = baseNum + 127;
-if (oppNum1 > 255) oppNum1 = oppNum1 - 255;
-let oppNum2 = baseNum2 + 127;
-if (oppNum2 > 255) oppNum2 = oppNum2 - 255;
-let oppNum3 = baseNum3 + 127;
-if (oppNum3 > 255) oppNum3 = oppNum3 - 255;
+// let oppNum1 = baseNum + 127;
+// if (oppNum1 > 255) oppNum1 = oppNum1 - 255;
+// let oppNum2 = baseNum2 + 127;
+// if (oppNum2 > 255) oppNum2 = oppNum2 - 255;
+// let oppNum3 = baseNum3 + 127;
+// if (oppNum3 > 255) oppNum3 = oppNum3 - 255;
 
 a.forEach(link => {
-  // link.style.backgroundColor = `rgb(${oppNum1}, ${baseNum2}, ${oppNum3})`;
   link.style.backgroundColor = "black";
   color = "white";
 });
 
 const sqArray = [[]];
 
-for (let i = 1; i < 30; i++) {
+for (let i = 1; i < 10; i++) {
   for (let j = 1; j < 13; j++) {
+    baseNum4 = Math.abs(j - 4) + Math.abs(i - 5);
+    const num = Math.random();
     sqArray.push(["hi"]);
     sqArray[i].push(hexy.cloneNode(true));
     let newSq = sqArray[i][j];
@@ -37,18 +38,36 @@ for (let i = 1; i < 30; i++) {
       newSq.style.color = "black";
       newSq.style.opacity = "20%";
     } else {
-      const num = Math.random();
       newSq.style.color = `rgb(
           ${Math.floor(baseNum + num * 80)},
           ${Math.floor(baseNum2 + num * 80)},
           ${Math.floor(baseNum3 + num * 80)})`;
+      newSq.style.filter = `brightness(200)`;
+      const keyFrameChoose = ["colorChange", "colorChange2"];
+      newSq.style.animationName = keyFrameChoose[Math.floor(num * 2)];
+      newSq.style.animationDuration = `${30 + num * 70}s`;
+      const direction = ["normal", "reverse"];
+      newSq.style.animationDirection = direction[Math.floor(num * 2)];
+      newSq.style.animationIterationCount = "infinite";
     }
     if (i % 4 !== 0 && (i - 1) % 4 !== 0) {
       newSq.style.position = "relative";
       newSq.style.left = "50%";
     }
+    // newSq.innerText = `${newSq.style.color}`;
+    // newSq.style.transform = "scaleY(1)";
     wrapper.appendChild(newSq);
   }
 }
 
 hexy.remove();
+
+//on scroll
+
+// window.onscroll = function() {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   console.log(hexy.scrollTop);
+// }
